04f51fb8c32f9648cfc7cb7f875dda9397d4ad47
francischung <francis.c.chung@gmail.com>
2019-04-16 11:40:52 +1000

    Validation in DTO added

--- DatingApp.API/DTO/UserForRegisterDTO.cs
+++ DatingApp.API/DTO/UserForRegisterDTO.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace DatingApp.API.DTO
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string  Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength=5, ErrorMessage="You must Specify password between 4 and 8")]
         public string Password { get; set; }
     }
 }
