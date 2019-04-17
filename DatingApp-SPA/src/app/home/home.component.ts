3141ebf5328a8b078b4bd54845511c547e56d4b9
francischung <francis.c.chung@gmail.com>
2019-04-17 15:15:31 +1000

    gitignore DatimngApp.API.dll & pdb added

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { registerModuleFactory } from '@angular/core/src/linker/ng_module_factory_loader';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,24 @@ import { registerModuleFactory } from '@angular/core/src/linker/ng_module_factor
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  values: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
+  getValues() {
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response;
+    },
+    error  => {
+      console.log(error);
+    });
+  }
 }
