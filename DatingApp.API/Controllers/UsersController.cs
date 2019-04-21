a4d066f4be5e690532012e061cd1c174cf7df40c
francischung <francis.c.chung@gmail.com>
2019-04-21 13:17:33 +1000

    New UsersController

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        public UsersController(IDatingRepository repo)
+        {
+            this._repo = repo;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+
+            return Ok(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser(int Id)
+        {
+            var user = await _repo.GetUser(Id);
+            return Ok(user);
+        }
+    }
+}
