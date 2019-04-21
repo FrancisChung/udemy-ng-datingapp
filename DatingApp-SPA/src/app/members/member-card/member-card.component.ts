74e74c3ec0101fdd7f546dbece44870fb1c4ae17
francischung <francis.c.chung@gmail.com>
2019-04-22 09:30:09 +1000

    Member Card Code

--- DatingApp-SPA/src/app/members/member-card/member-card.component.ts
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-member-card',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-card.component.scss']
 })
 export class MemberCardComponent implements OnInit {
+  @Input()  user:User;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
