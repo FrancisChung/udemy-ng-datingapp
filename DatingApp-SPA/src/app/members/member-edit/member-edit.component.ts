347ed6308e279cf514f70dddb0869a16f36e4f76
francischung <francis.c.chung@gmail.com>
2019-06-16 09:05:31 +1000

    Use AuthService Subscription for PhotoUrl

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -17,6 +17,7 @@ export class MemberEditComponent implements OnInit {
     private userService: UserService, private authService: AuthService) { }
   @ViewChild('editForm') editForm: NgForm;
   user: User;
+  photoUrl: string;
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
     if (this.editForm.dirty) {
@@ -27,6 +28,7 @@ export class MemberEditComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data.user;
+      this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
     });
   }
 
