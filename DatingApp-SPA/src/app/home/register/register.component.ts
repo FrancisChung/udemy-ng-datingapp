4ccc8a17598964fd9deffc4c0dbee86a278a9fce
francischung <francis.c.chung@gmail.com>
2019-06-17 23:15:26 +1000

    ReactiveForm Skeleton code

--- DatingApp-SPA/src/app/home/register/register.component.ts
+++ DatingApp-SPA/src/app/home/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,19 +11,28 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword : new FormControl()
+    })
   }
 
   register() {
+    /**
     this.authService.register(this.model).subscribe(()=>{
       this.alertify.success("Registration successful");
     },
     error => {
       this.alertify.error(error);
     });
+    */
+   console.log(this.registerForm.value);
   }
 
   cancel() {
