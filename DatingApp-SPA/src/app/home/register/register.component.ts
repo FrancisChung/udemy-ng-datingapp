4c4506682eb2345dd66423ff8e27206ba32a9fcf
francischung <francis.c.chung@gmail.com>
2019-06-23 03:20:33 +1000

    New Register functionality

--- DatingApp-SPA/src/app/home/register/register.component.ts
+++ DatingApp-SPA/src/app/home/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from 'src/app/_models/user';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,11 +12,12 @@ import { BsDatepickerConfig } from 'ngx-bootstrap';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
+  constructor(private authService: AuthService, private router: Router,
+    private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.bsConfig = {
@@ -41,14 +44,19 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    /**
-    this.authService.register(this.model).subscribe(()=>{
-      this.alertify.success("Registration successful");
-    },
-    error => {
-      this.alertify.error(error);
-    });
-    */
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Registration Successful');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/members']);
+        })
+      })
+    }
+
    console.log(this.registerForm.value);
   }
 
